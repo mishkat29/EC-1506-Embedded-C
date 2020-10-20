d8e434fb63e58177ce00f34d5f81027de0157128
Mishkat Ahmad Neyazi <49838423+mishkat29@users.noreply.github.com>
2020-10-20 23:04:46 +0530

    Add files via upload

---
+++ Count_min_n_sec.c
@@ -0,0 +1,34 @@
+ /*
+ By- Mishkat
+ 60-Hz external clock is being fed into pin TO (P3.4). 
+ program for counter 0 in mode 2 (8-Bit Auto-reload) to display the seconds and minutes on PI and P2, respectively.
+ */
+ #include<reg51.h>
+ void secmin(unsigned char);
+ void main()
+ {
+	 unsigned char val;
+	 T0=1;
+	 TMOD=0x06;
+	 TH0=-60;
+	 while(true)
+	 {
+		 do
+		 {
+			 TR0=1;
+			 val=TL0;
+			 secmin(val);
+		 }
+		 while(TF0==0);
+		 TR0=0;
+		 TF0=0;
+	 }
+ }
+ void secmin(unsigned char val)
+ {
+	 unsigned char sec, min;
+	 min= val/60;
+	 sec= val%60;
+	 P1=sec;
+	 P2=min;
+ }
